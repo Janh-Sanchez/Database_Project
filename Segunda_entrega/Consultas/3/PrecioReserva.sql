26ed39d0702b0190e8fd2caee03cc1536158c570
DarkJans19 <jansito4221@gmail.com>
2025-07-10 09:36:31 -0500

    Add the principal project

--- Segunda_entrega/Consultas/3/PrecioReserva.sql
+++
@@ -1,7 +0,0 @@
-DROP VIEW IF EXISTS PrecioReserva;
-CREATE VIEW PrecioReserva AS
-SELECT DNI, idHabitacion, fechaLlegada, fechaSalida, CantidadDeNoches,
-precioCategoria * CantidadDeNoches AS precioReserva
-FROM cantidadNoches NATURAL JOIN Categoria NATURAL JOIN Habitacion;
-
-SELECT * FROM PrecioReserva
